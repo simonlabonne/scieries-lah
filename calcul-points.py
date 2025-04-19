a94db1c6a00d1af1b6edac4e2b668a8738bb47fc
Simon Labonne <simon.labonne@usherbrooke.ca>
2025-04-19 16:18:23 -0400

    Testing a new setup for python export

--- calcul-points.py
+++ calcul-points.py
@@ -1,7 +1,4 @@
 import pandas as pd
 teams = pd.read_csv('Fantrax-Team-Roster-League Against Humanity.csv')
 teams['points'] = 0
-teams_json = teams.to_json('src/app/scieries.json', orient='records')
-# dump json
-with open('src/app/scieries.json', 'w') as f:
-    f.write(teams_json)
+teams.to_json('scieries.json', orient='records')
