d096cea6a5f7ee5c6b9932fdf73f59e64e221136
Simon Labonne <simon.labonne@usherbrooke.ca>
2025-04-19 16:11:18 -0400

    Testing a new fix

--- calcul-points.py
+++ calcul-points.py
@@ -2,3 +2,4 @@ import pandas as pd
 teams = pd.read_csv('Fantrax-Team-Roster-League Against Humanity.csv')
 teams['points'] = 0
 teams.to_json('src/app/scieries.json', orient='records')
+print(teams)
