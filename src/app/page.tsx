af9af506786c4ab8ede9a0502e91fe4b5aeea976
Simon Labonne <simon.labonne@usherbrooke.ca>
2025-05-05 19:40:53 -0400

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,8 +61,8 @@ export default function Page() {
   }
 
   return (
-    <div className="container mx-auto mt-8">
-      <img src="pub-1.jpg" alt="Advertisement" className="mb-8" />
+    <div className="container mx-auto mt-8 px-2 lg:px-0">
+      <img src="pub-1.jpg" alt="Advertisement" className="mx-auto mb-8" />
       <h1 className="text-4xl mb-4">Scieries LAH</h1>
       <div className="grid lg:grid-cols-2 gap-4">
         <div><TeamPlayers id={1} data={bons} /></div>
