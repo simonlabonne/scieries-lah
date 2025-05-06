03fffd2c2aabaecfce9f8e2f50a31ae0341bed4e
Simon Labonne <simon.labonne@usherbrooke.ca>
2025-05-06 07:58:29 -0400

    messed up the dates

--- calcul-points.py
+++ calcul-points.py
@@ -57,30 +57,28 @@ start = 0
 start_date = '2025-05-05'
 
 while True:
-    data = get_data("https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=true&isGame=true&sort=%5B%7B%22property%22:%22points%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22goals%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22assists%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start="+str(start)+"&limit=100&cayenneExp=gameDate%3C=%22"+start_date+"%2023%3A59%3A59%22%20and%20gameDate%3E=%222034-10-28%22%20and%20gameTypeId=3")
+    data = get_data("https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=true&isGame=true&sort=%5B%7B%22property%22:%22points%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22goals%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22assists%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start="+str(start)+"&limit=100&cayenneExp=gameDate%3C=%222034-10-28%2023%3A59%3A59%22%20and%20gameDate%3E=%22"+start_date+"%22%20and%20gameTypeId=3")
     compiler.extend(data['data'])
     if len(data['data']) < 100:
         break
     start += 100
 start = 0
 while True:
-    data = get_data("https://api.nhle.com/stats/rest/en/goalie/summary?isAggregate=true&isGame=true&sort=%5B%7B%22property%22:%22wins%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22savePct%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start="+str(start)+"&limit=100&cayenneExp=gameDate%3C=%22"+start_date+"%2023%3A59%3A59%22%20and%20gameDate%3E=%222034-10-28%22%20and%20gameTypeId=3")
+    data = get_data("https://api.nhle.com/stats/rest/en/goalie/summary?isAggregate=true&isGame=true&sort=%5B%7B%22property%22:%22wins%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22savePct%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start="+str(start)+"&limit=100&cayenneExp=gameDate%3C=%222034-10-28%2023%3A59%3A59%22%20and%20gameDate%3E=%22"+start_date+"%22%20and%20gameTypeId=3")
     compiler.extend(data['data'])
     if len(data['data']) < 100:
         break
     start += 100
     
 nhl_stats = pd.DataFrame(compiler)
-try:
-    nhl_stats['skaterFullName'] = nhl_stats['skaterFullName'].fillna(nhl_stats['goalieFullName'])
-    nhl_stats = nhl_stats.drop(['points'], axis=1)
+nhl_stats['skaterFullName'] = nhl_stats['skaterFullName'].fillna(nhl_stats['goalieFullName'])
+nhl_stats = nhl_stats.drop(['points'], axis=1)
 
-    df_merge = pd.merge(nhl_stats, bons, how="outer", left_on="skaterFullName", right_on="player")
-    df_merge = df_merge.dropna(subset=['player'])
-    df_merge['points'] = df_merge.apply(calculate_points, axis=1)
-    df_merge = df_merge.sort_values(by=['points'], ascending=False)
-    df_merge['points'] = df_merge['points'].fillna(0)
+df_merge = pd.merge(nhl_stats, bons, how="outer", left_on="skaterFullName", right_on="player")
+df_merge = df_merge.dropna(subset=['player'])
+df_merge['points'] = df_merge.apply(calculate_points, axis=1)
+df_merge = df_merge.sort_values(by=['points'], ascending=False)
+df_merge['points'] = df_merge['points'].fillna(0)
 
-    df_merge.to_json('scieries-bons.json', orient='records')
-except:
-    bons.to_json('scieries-bons.json', orient='records')
+df_merge.to_json('scieries-bons.json', orient='records')
+bons.to_json('scieries-bons.json', orient='records')
